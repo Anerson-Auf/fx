d77a763798ae576db737d003851c051e2a984a75
Anerson <61119210+Anerson-Auf@users.noreply.github.com>
2026-04-10 21:34:32 +0300

    Implement buffered file download with progress tracking
    Replaced the copy function with a buffered read and write for downloading files, adding progress tracking.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use sevenz_rust::decompress_file;
 use rfd::FileDialog;
 use color_eyre::{eyre::Result};
 use reqwest::blocking::Client;
-use std::io::{copy};
+use std::io::{Read, Write};
 use std::fs::{File, create_dir_all};
 use std::path::Path;
+use std::time::{Duration, Instant};
 
 const URLS: [(&str, &str); 3] = [
     ("https://github.com/Anerson-Auf/fx/releases/download/q/ToWRR.7z", ""), // game folder
@@ -23,19 +24,42 @@ fn main() -> Result<()> {
     }
     let game_path = FileDialog::new().pick_folder().unwrap();
     let client = Client::new();
-
+    let mut last_update = Instant::now();
     for (url, sub_path) in URLS {
         let mut resp = client.get(url).send()?.error_for_status()?;
-
+        
         let dest_dir = game_path.join(sub_path);
         create_dir_all(&dest_dir)?;
-
+        
         let archive_path = dest_dir.join("temp.7z");
-
+        
         let mut file = File::create(&archive_path)?;
-        copy(&mut resp, &mut file)?;
+
+        let total_size = resp.content_length().unwrap_or(0);
+        let mut downloaded: u64 = 0;
+        
+        let mut buffer = [0; 8192];
+        loop {
+            let n = resp.read(&mut buffer)?;
+            if n == 0 {
+                break;
+            }
+            file.write_all(&buffer[..n])?;
+            downloaded += n as u64;
+            if total_size > 0 && last_update.elapsed() >= Duration::from_millis(100) {
+                print!("\r[{:?}] Progress: {:.2}%", sub_path, (downloaded as f64 / total_size as f64) * 100.0);
+                std::io::stdout().flush().unwrap();
+                last_update = Instant::now();
+            }
+        }
+        if downloaded == total_size {
+            println!();
+            println!("[{:?}] Successfully downloaded.", sub_path);
+        }
+        //copy(&mut resp, &mut file)?;
 
         decompress_file(&archive_path, &dest_dir)?;
+        println!("[{:?}] Successfully decompress.", sub_path);
 
         std::fs::remove_file(&archive_path)?;
     }
